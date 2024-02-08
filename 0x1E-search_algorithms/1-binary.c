c8d0dd2bf2ba10f7387b86d889fec57c2db7ffc1
Michyle <michyleramdass@gmail.com>
2024-02-08 05:16:20 -0800

    push

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,60 +1,39 @@
 #include "search_algos.h"
 
 /**
- * recursive_search - this will searche for a value in an
- * array of integers using the Binary search algorithm
- *
- * @array: input array
- * @size: the size of the array
- * @value: the value to search for
- * Return: The index of the number
- */
-int recursive_search(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in an array");
-
-	for (i = 0; i < size; i++)
-		printf("%s" "%d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
-}
-
-/**
  * binary_search - this will call to binary_search to 
- * return the index of the number
- *
- * @array: input array
+ * search for a value in a sorted array
+ * @array: a pointer to the first element of the array to search for
  * @size: the size of the array
  * @value: the value to search for
- * Return: index of the number
+ * Return: -1 if the value is not presnt of the array is NULL, 
+ * Else return the index of the item
+ *
+ * description: print the sub array
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+	size_t i, left, right;
 
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
+	if (array == NULL)
 		return (-1);
 
-	return (index);
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[1] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
 }
